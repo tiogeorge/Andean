11de26b418e6ceccf064b3b27a2db7091ded6b1d
CesarAchahuiVilca <130341@unsaac.edu.pe>
2018-10-19 14:38:01 -0500

    prueba componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import {RouterModule, Route} from '@angular/router';
+import { HomeComponent } from './home/home.component'
+
+const routes : Route[] = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
