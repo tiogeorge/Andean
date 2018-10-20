0ffd4a59c290997294c31b03faee1a53ef10cb58
CesarAchahuiVilca <130341@unsaac.edu.pe>
2018-10-20 13:26:38 -0500

    avance login

--- src/app/material.ts
+++ src/app/material.ts
@@ -2,9 +2,11 @@ import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material';
+import {MatChipsModule} from '@angular/material/chips';
+
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
-  exports: [MatButtonModule, MatCheckboxModule,MatToolbarModule, MatIconModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatChipsModule],
+  exports: [MatButtonModule, MatCheckboxModule,MatToolbarModule, MatIconModule, MatChipsModule],
 })
 export class MaterialModule { }
