f1d9c6ca6bea3332cd600148d9cf94763382b188
Abel Gabriel Zapata <zapata.gabriel.abel@gmail.com>
2018-10-25 09:31:21 -0500

    Eliminar archivo de configuración de Base de Datos
    Eliminar el archivo de configuración de la base de datos.

--- server/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const URI = 'mongodb://el_ornitorrinco:1nadando@ds241723.mlab.com:41723/andeanstore';
-
-mongoose.connect(URI)
-.then(db => console.log('Base de datos esta conectada..'))
-.catch(err=> console.log(err));
-
-module.exports = mongoose;
-
