5207d2dc73a105176c225dce416813e4ce112b8e
CesarAchahuiVilca <130341@unsaac.edu.pe>
2018-10-29 13:43:30 -0500

    componente detalle-articulo
    avance zoom imagen

--- src/app/componentes/articulo-detalle/articulo-detalle.component.ts
+++ src/app/componentes/articulo-detalle/articulo-detalle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-articulo-detalle',
@@ -10,6 +11,49 @@ export class ArticuloDetalleComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  mouse_over(){
+
+    let cont = document.getElementById("cont-imagen-zoom") as HTMLDivElement;
+    let imagen = document.getElementById("imagen-zoom") as HTMLDivElement;
+    let imageseleccionada = document.getElementById("imagen-seleccionada") as HTMLImageElement;
+    let contenedor = document.getElementById("imagen-grande-select") as HTMLDivElement;
+    cont.style.width = (imageseleccionada.clientWidth+10)+"px";
+    cont.style.height = (imageseleccionada.clientHeight+10)+"px";
+    imagen.style.backgroundImage = "url('"+imageseleccionada.src+"')";
+    imagen.style.zIndex="-100";
+    imagen.style.transform="scale(3)";
+    cont.style.display="block";
+
+  }
+  mouse_out(){
+    let cont = document.getElementById("cont-imagen-zoom") as HTMLDivElement;
+    let imagen = document.getElementById("imagen-zoom") as HTMLImageElement;
+    let imageseleccionada = document.getElementById("imagen-seleccionada") as HTMLImageElement;
+    let contenedor = document.getElementById("imagen-grande-select") as HTMLDivElement;
+    cont.style.width = (imageseleccionada.clientWidth+10)+"px";
+    cont.style.height = (imageseleccionada.clientHeight+10)+"px";
+    imagen.style.backgroundImage = imageseleccionada.src;
+    imagen.style.transform="scale(1)";
+    cont.style.width = contenedor.clientWidth+"px";
+    cont.style.display="none";
+  }
+  mouse_move(){
+    let cont = document.getElementById("cont-imagen-zoom") as HTMLDivElement;
+    let imagen = document.getElementById("imagen-zoom") as HTMLDivElement;
+    let imageseleccionada = document.getElementById("imagen-seleccionada") as HTMLImageElement;
+    let contenedor = document.getElementById("imagen-grande-select") as HTMLDivElement;
+  
+    var x = (event as MouseEvent).pageX;
+    var y = (event as MouseEvent).pageY;
+    var ancho = imageseleccionada.clientWidth;
+    var altopor = ((y - imageseleccionada.offsetTop) / imageseleccionada.clientHeight) * 100 +'%';
+
+    imagen.style.transformOrigin= (((x - imageseleccionada.offsetLeft) / imageseleccionada.clientWidth* 100) )+ "% " +((((y - imageseleccionada.offsetTop) / imageseleccionada.clientHeight) * 100) - 100) +'%';
+    console.log(altopor);
+    
   }
 
 }
--- src/assets/prueba.js
+++
@@ -1,9 +0,0 @@
-function square(x) {
-    return Math.pow(x,2)
-}
-
-function cow() {
-    console.log("Mooooo!!!")
-}
-
-export {square, cow};
