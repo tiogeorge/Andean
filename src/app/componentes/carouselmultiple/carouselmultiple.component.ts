d0c72fece4cf97b671444187dc3b6eb4be51e1bd
paultbr <paultheo65@gmail.com>
2018-11-05 10:15:26 -0500

    multicar

--- src/app/componentes/carouselmultiple/carouselmultiple.component.ts
+++ src/app/componentes/carouselmultiple/carouselmultiple.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import { $ } from 'protractor';
 
 @Component({
   selector: 'app-carouselmultiple',
   templateUrl: './carouselmultiple.component.html',
-  styleUrls: ['./carouselmultiple.component.css']
+  styleUrls: ['./carouselmultiple.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CarouselmultipleComponent implements OnInit {
-
+  listaofertaarti:string[]=['articulo 0','articulo 1','articulo 2','articulo 3','articulo 4','articulo 5','articulo 6','articulo 7']
   constructor() { }
 
   ngOnInit() {
+  }
+  prueba(){
+    console.log('entra');
   }
 
 }
