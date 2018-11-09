b7c41e9db14552079df2b829d66bbcbe621d7e67
paultbr <paultheo65@gmail.com>
2018-11-09 11:59:44 -0500

    eliminar

--- src/app/carritocompras/carritocompras.component.ts
+++ src/app/carritocompras/carritocompras.component.ts
@@ -1,31 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { post } from "selenium-webdriver/http";
 
 @Component({
-  selector: 'app-carritocompras',
-  templateUrl: './carritocompras.component.html',
-  styleUrls: ['./carritocompras.component.css']
+  selector: "app-carritocompras",
+  templateUrl: "./carritocompras.component.html",
+  styleUrls: ["./carritocompras.component.css"]
 })
 export class CarritocomprasComponent implements OnInit {
+  listaarticuloscart: string[] = ["Articulo 1", "Artiulo 2", "Articulo 3"];
+  constructor() {}
 
-  listaarticuloscart: string[] = ['Articulo 1', 'Artiulo 2', 'Articulo 3'];
-  constructor() { }
-
-  ngOnInit() {
-  }
-  mostrardivenvio(){
-    if(document.getElementById('listaenvio').style.display == 'block'){
-      document.getElementById('listaenvio').style.display = 'none';
-    }
-    else{
-      document.getElementById('listaenvio').style.display = 'block';
+  ngOnInit() {}
+  mostrardivenvio() {
+    if (document.getElementById("listaenvio").style.display == "block") {
+      document.getElementById("listaenvio").style.display = "none";
+    } else {
+      document.getElementById("listaenvio").style.display = "block";
     }
   }
-  mostrardivcupon(){
-    if(document.getElementById('seltcupon').style.display == 'block'){
-      document.getElementById('seltcupon').style.display = 'none';
+  mostrardivcupon() {
+    if (document.getElementById("seltcupon").style.display == "block") {
+      document.getElementById("seltcupon").style.display = "none";
+    } else {
+      document.getElementById("seltcupon").style.display = "block";
     }
-    else{
-      document.getElementById('seltcupon').style.display = 'block';
+  }
+  eliminaritem(dato) {
+    var pos=this.listaarticuloscart.indexOf(dato);
+    this.listaarticuloscart.splice(pos,1);
+    console.log(pos);
+    console.log(this.listaarticuloscart);
+    this.mencart();
+  }
+  eliminartodo(){
+    this.listaarticuloscart=null;
+    this.mencart();
+  }
+  mencart(){
+    if(this.listaarticuloscart==null){
+      document.getElementById('mencartvacio').style.display=('block');
+      document.getElementById('listacartarticulos').style.display=('none');
     }
   }
 }
