d57c9b31f7865ab708d6d2f0951708447b47a9cd
gabel10 <zapata.gabriel.abel@gmail.com>
2018-11-12 14:39:27 -0500

    Mantenimiento Usuarios
    CRUD de Usuarios

--- server/controllers/usuario.controller.js
+++ server/controllers/usuario.controller.js
@@ -3,6 +3,34 @@ const bcrypt = require('bcrypt');
 const usuarioController = {};
 const salt = bcrypt.genSaltSync();
 
+usuarioController.actualizarUsuario = async (req, res, next) => {
+  try {
+    const user = {
+      nombres: req.body.nombres,
+      apellidos: req.body.apellidos,
+      promociones: req.body.promociones
+    }
+    const usuario = await Usuario.findOneAndUpdate({
+      id: req.params.id
+    }, {
+      $set: user
+    }, {
+      new: false
+    });
+    res.json({
+      status: 1,
+      mensaje: "Usuario actualizado"
+    });
+
+  } catch (err) {
+    res.json({
+      status: 0,
+      mensaje: " No se pudo actulizar los datos de la categoria : ERROR:" + err
+
+    });
+  }
+};
+
 usuarioController.crearUsuario = async (req, res, next) => {
   const user = await Usuario.find({
     correo: req.body.correo
@@ -27,6 +55,57 @@ usuarioController.crearUsuario = async (req, res, next) => {
       msg: 'Usuario creado'
     });
   }
+};
+
+usuarioController.eliminarUsuario = async (req, res, next) => {
+  try {
+    const user = await Usuario.remove({
+      id: req.params.id
+    });
+    res.json({
+      status: 1,
+      mensaje: "Se eliminó el usuario"
+    });
+
+  } catch (err) {
+    res.json({
+      status: 0,
+      mensaje: " No se pudo eliminar al usuario : ERROR:" + err
+
+    });
+  }
+};
+
+usuarioController.listarUsuarios = async (req, res, next) => {
+  const usuarios = await Usuario.find();
+  res.json(usuarios);
+};
+
+usuarioController.loginAdmin = async (req, res, next) => {
+  req.getConnection(function (error, conn) {
+    var consulta = "call spuPrin_IniciarSesionUsuario(?,?,?)";
+    var valores = [req.body.usuario.toUpperCase(), req.body.password, 'web'];
+    conn.query(consulta, valores, function (err, results) {
+      if (err) throw err
+      // if user not found
+      // Se obtiene mensaje, idEmpleado, Nombres, idTipoUusario
+      if (results[0][0].Mensaje == 'HECHO') {
+        req.session.cont = req.session.cont ? req.session.cont + 1 : 1;
+        req.session.idEmpleado = req.session.idEmpleado ? req.session.idEmpleado : results[0][0].idEmpleado;
+        req.session.usuario = req.session.usuario ? req.session.usuario : results[0][0].Nombres;
+        req.session.idTipoUsuario = req.session.idTipoUsuario ? req.session.idTipoUsuario : results[0][0].idTipoUsuario;
+        res.json({
+          status: 'Iniciando sesión',
+          estado: true
+        });
+      } else {
+        res.json({
+          status: 'Usuario y/o contraseña incorrecta',
+          estado: false
+        });
+      }
+    })
+  });
 };
 
 usuarioController.loginUsuario = async (req, res, next) => {
@@ -55,29 +134,13 @@ usuarioController.loginUsuario = async (req, res, next) => {
       });
     }
   }
-}
+};
 
-usuarioController.loginAdmin = async (req, res, next) => {
-  req.getConnection(function (error, conn) {
-    var consulta = "call spuPrin_IniciarSesionUsuario(?,?,?)";
-    var valores = [req.body.usuario, req.body.password, 'web'];
-    conn.query(consulta, valores, function(err, rows, fields) {
-      if(err) throw err        
-      // if user not found
-      // Se obtiene mensaje, idEmpleado, Nombres, idTipoUusario
-      if (results[0] == 'HECHO'){
-        res.json({
-          status: 'Iniciando sesión',
-          estado: true
-        });
-      }else {
-        res.json({
-          status: 'Usuario y/o contraseña incorrecta.',
-          estado: false
-        });
-      }
-    })
+usuarioController.obtenerUsuario = async (req, res, next) => {
+  const usuario = await Usuario.find({
+    id: req.params.id
   });
-}
+  res.json(usuario);
+};
 
 module.exports = usuarioController;
--- server/routes/usuario.routes.js
+++ server/routes/usuario.routes.js
@@ -6,5 +6,9 @@ const usuario = require('../controllers/usuario.controller');
 router.post('/', usuario.crearUsuario);
 router.post('/login', usuario.loginUsuario);
 router.post('/adminLogin', usuario.loginAdmin);
+router.get('/', usuario.listarUsuarios);
+router.put('/:id',usuario.actualizarUsuario);
+router.get('/:id',usuario.obtenerUsuario);
+router.delete('/:id',usuario.eliminarUsuario);
 
 module.exports = router;
