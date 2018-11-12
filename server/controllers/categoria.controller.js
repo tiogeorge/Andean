f2f1302bf7dfde546d018263e812e4b171033ffa
CesarAchahuiVilca <130341@unsaac.edu.pe>
2018-11-12 10:09:20 -0500

    CRUD categorias

--- server/controllers/categoria.controller.js
+++ server/controllers/categoria.controller.js
@@ -27,11 +27,44 @@ categoriaController.crearCategoria = async (req, res) => {
     }
 };
 categoriaController.actualizarCategoria = async(req,res)=>{
-    const categoria = await Categoria.find({
-        id:req.params.id
-    });
-    res.json(categoria);
+    try{
+        const cat = {
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            imagen: req.body.imagen,
+            padre: req.body.padre
+        }
+        const categoria = await Categoria.findOneAndUpdate({id:req.params.id},{$set: cat},{new: true});        
+        res.json({
+            status:1,
+            mensaje:"Se actuAlizaron los datos correctamente"
+        });       
+        
+    }catch(err){
+        res.json({
+            status:0,
+            mensaje:" No se pudo actulizar los datos de la categoria : ERROR:"+ err                  
+            
+        });
+    }
 
+}
+categoriaController.eliminarCategoria = async (req,res)=>{
+    try{
+        
+        const categoria = await Categoria.remove({id:req.params.id});        
+        res.json({
+            status:1,
+            mensaje:"Se elimino los datos correctamente"
+        });       
+        
+    }catch(err){
+        res.json({
+            status:0,
+            mensaje:" No se pudo eliminar los datos de la categoria : ERROR:"+ err                  
+            
+        });
+    }
 }
 
 
