1f77d8aeb1df4658180a71f7fb3183c354218371
paultbr <paultheo65@gmail.com>
2018-11-21 13:54:16 -0500

    marcav2-server

--- server/controllers/marca.controller.js
+++ server/controllers/marca.controller.js
@@ -16,7 +16,38 @@ marcacontroller.obtenerMarcaMysql = async( req, res)=>{
     
 }
 
-marcacontroller.getMarcas= async (req, res) =>{
+marcacontroller.obtenerMarcas= async (req, res) =>{
     const marcas = await Marca.find();
     res.json(marcas);
 }
+
+marcacontroller.crearMarca= async (req,res)=>{
+    const marca=new Marca({
+        nombremarca:req.body.nombremarca
+    });
+    await marca.save();
+    res.json({
+        'status': 'Marca registrada'
+    });
+}
+
+marcacontroller.obtenermarca= async (req,res)=>{
+    const marca= await Marca.findById(req.params.id);
+    res.json(marca);
+}
+
+marcacontroller.editarmarca= async (req,res)=>{
+    const {id}=req.params;
+    const marca={
+        nombremarca:req.body.nombremarca
+    };
+    await Employee.findByIdAndUpdate(id,{$set:employee},{new:true});
+    res.json({'status':'Marca Actualizada'});
+}
+
+marcacontroller.eliminar=async (req,res)=>{
+    await Marca.findByIdAndRemove(req.params.id);
+    res.json({'status':'Marca Eliminada'})
+}
+
+module.exports=marcacontroller;
