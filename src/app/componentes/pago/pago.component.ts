0a6ad837f50619a457eaeee615b4d2deb71f21f3
paultbr <paultheo65@gmail.com>
2018-12-05 08:42:24 -0500

    pagov3

--- src/app/componentes/pago/pago.component.ts
+++ src/app/componentes/pago/pago.component.ts
@@ -1,15 +1,123 @@
 import { Component, OnInit } from '@angular/core';
+import {MatChipInputEvent} from '@angular/material';
 
+export interface NombreDirec {
+  nombre: string;
+}
+
+export interface Años {
+  value: string;
+  viewValue: string;
+}
+export interface Mes {
+  value: string;
+  viewValue: string;
+}
+export interface Tipolocalenvio {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-pago',
   templateUrl: './pago.component.html',
   styleUrls: ['./pago.component.css']
 })
+
 export class PagoComponent implements OnInit {
+  //chips
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  fruits: NombreDirec[] = [
+    {nombre: 'Direccion1'},
+  ];
+  //
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.fruits.push({nombre: value.trim()});
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  direccionselec(id:string){
+    document.getElementById(id).style.background='#FFBF00';
+    document.getElementById(id).style.color='white';
+  }
+  remove(fruit: NombreDirec): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
+  //fin chips
+  Local: Tipolocalenvio[] = [
+    { value: 'Tipo1', viewValue: 'Casa' },
+    { value: 'Tipo2', viewValue: 'Oficina' },
+    { value: 'Tipo3', viewValue: 'Departamento' },
+    { value: 'Tipo4', viewValue: 'Edificio' },
+    { value: 'Tipo5', viewValue: 'Condominio' },
+    { value: 'Tipo16', viewValue: 'Otro' }
+    
+  ];
+  Anhos: Años[] = [
+    { value: '2018', viewValue: '2018' },
+    { value: '2019', viewValue: '2019' },
+    { value: '2020', viewValue: '2020' },
+    { value: '2021', viewValue: '2021' },
+    { value: '2022', viewValue: '2022' },
+    { value: '2023', viewValue: '2023' },
+    { value: '2024', viewValue: '2023' }
+  ];
+  mes: Mes[] = [
+    { value: '1', viewValue: '1' },
+    { value: '2', viewValue: '2' },
+    { value: '3', viewValue: '3' },
+    { value: '4', viewValue: '4' },
+    { value: '5', viewValue: '5' },
+    { value: '6', viewValue: '6' },
+    { value: '7', viewValue: '7' },
+    { value: '8', viewValue: '8' },
+    { value: '9', viewValue: '9' },
+    { value: '10', viewValue: '10' },
+    { value: '11', viewValue: '11' },
+    { value: '12', viewValue: '12' }
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    document.getElementById('datostarjeta').hidden = true;
+    document.getElementById('Agregardireccion').hidden = true;
+  }
+  mostrarform(value: string) {
+    console.log(value);
+    if (value == '1') {
+      document.getElementById('datostarjeta').hidden = false;
+    }
+    if (value == '2') {
+      document.getElementById('datostarjeta').hidden = true;
+    }
+    if (value == '3') {
+      document.getElementById('datostarjeta').hidden = true;
+    }
+  }
+  mostrarformdir() {
+    if (document.getElementById('Agregardireccion').hidden == true) {
+      document.getElementById('Agregardireccion').hidden = false;
+    }
+    else{
+      document.getElementById('Agregardireccion').hidden = true;
+    }
   }
 
 }
