6500e1c9cc123ee62ff95ed9c5cbf4bdd5fb204d
CesarAchahuiVilca <130341@unsaac.edu.pe>
2019-01-23 14:23:18 -0500

    compresion imagenes

--- server/controllers/imagen.controller.js
+++ server/controllers/imagen.controller.js
@@ -8,7 +8,9 @@ const pngquant = require('imagemin-pngquant');
 const mozjpeg = require('imagemin-mozjpeg');
 const input= './imagenes/tmp';
 const output = './imagenes/compressed';
-
+const output_sm = './imagenes/sm';
+const output_md = './imagenes/md';
+const output_lg = './imagenes/lg';
 
 
 
@@ -17,31 +19,42 @@ const output = './imagenes/compressed';
 const imageCompressorRun = (input, output, plugins) => {
     return imagemin(input, output, { plugins });
 }
-const compressorPlugins = [
-    mozjpeg({ speed: 1, quality: '50' }),
-    pngquant({ speed: 1, quality: '50' })
+const compressorPluginsLG = [
+    mozjpeg({ speed: 1, quality: '60' }),
+    pngquant({ speed: 1, quality: '60' })
 ];
-
+const compressorPluginsMD = [
+    mozjpeg({ speed: 3, quality: '40' }),
+    pngquant({ speed: 3, quality: '40' })
+];
+const compressorPluginsSM = [
+    mozjpeg({ speed: 3, quality: '20' }),
+    pngquant({ speed: 3, quality: '20' })
+];
 
 const imagenController = {};
 
 
 
 imagenController.subirImagen = function (req,res){
-
-    res.json({
-        estado: 1 ,
-        mensaje: 'Imagen subida'
-      });
+    console.log(req.file);
+    
     //Comprimir imagen
-    /*imageCompressorRun([`${input}/*.{jpg,jpeg,png}`], output, compressorPlugins)
+    //imageCompressorRun([`${input}/*.{jpg,jpeg,png}`], output, compressorPlugins)
+    imageCompressorRun([input+"/"+req.file.originalname], output_lg, compressorPluginsLG)
     .then(()=>{
-        console.log("se comprimio correctamente");
-        res.json({
-            exito: true,
-            msg: 'Imagen subida y comprimida'
-          });
-    });*/
+        imageCompressorRun([input+"/"+req.file.originalname], output_md, compressorPluginsMD)
+        .then(()=>{
+            imageCompressorRun([input+"/"+req.file.originalname], output_sm, compressorPluginsSM)
+            .then(()=>{
+                console.log("Imagenes comprimidas");
+                res.json({
+                    estado:1,
+                    mensaje:"Se comprimio la imagen correctamente"
+                });
+            });
+        });
+    });
 
 }
 imagenController.getImagenes = async (req,res)=>{
