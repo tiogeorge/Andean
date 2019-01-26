afe82b1008d7051078566fa1434e4de92502ac22
paultbr <paultheo65@gmail.com>
2019-01-26 11:06:38 -0500

    pago

--- server/controllers/pago.controller.js
+++ server/controllers/pago.controller.js
@@ -1,60 +1,73 @@
-const Pago =require('../models/pago');
+const Pago = require('../models/pago');
+const Clientecontroller = require('../controllers/usuario.controller');
+const Cliente = require('../models/usuario');
 
-const pagoController={};
+const pagoController = {};
 
-pagoController.GuardarPago=async(req,res)=>{
-    try{
-        const pago =new Pago(req.body);
+pagoController.GuardarPago = async (req, res) => {
+    try {
+        const pago = new Pago(req.body);
         console.log(req.body);
-        if(pago){
+        if (pago) {
             await pago.save();
             res.json({
-                mensaje:"Se guardo el pago"
+                mensaje: "Se guardo el pago"
             });
         }
-        else{
+        else {
             res.json({
-                mensaje:"ERROR"
+                mensaje: "ERROR"
             });
         }
-    } catch(e){
+    } catch (e) {
         res.json({
-            mensaje:"ERROR:"+e
+            mensaje: "ERROR:" + e
         })
     }
 }
 
-pagoController.listarpedidos=async (req,res)=>{
-    const pedidos=await Pago.find();
+pagoController.listarpedidos = async (req, res) => {
+    //const correoclient;
+/*    db.pagos.aggregate({
+        $lookup:
+        {
+            from: "usuarios",
+            localField: "idUsuario",
+            foreignField: "_id",
+            as: "datosclient"
+        }
+    })*/
+    const pedidos = await Pago.find();
+    console.log(pedidos);
     res.json(pedidos);
 }
 
-pagoController.listarpedidouni=async (req,res)=>{
-    const pedido= await Pago.findById(req.params.id);
+pagoController.listarpedidouni = async (req, res) => {
+    const pedido = await Pago.findById(req.params.id);
     res.json(pedido);
 }
 
-pagoController.actualizarpedido=async (req,res)=>{
-    try{
-        const art={
-            EstadoPago:req.body.EstadoPago,
-            EstadoEnvio:req.body.EstadoEnvio,
-            Documento:req.body.Documento
+pagoController.actualizarpedido = async (req, res) => {
+    try {
+        const art = {
+            EstadoPago: req.body.EstadoPago,
+            EstadoEnvio: req.body.EstadoEnvio,
+            Documento: req.body.Documento
         }
-        const pedido = await Pago.findByIdAndUpdate({_id:req.params.id},{$set:art},{new: true});
+        const pedido = await Pago.findByIdAndUpdate({ _id: req.params.id }, { $set: art }, { new: true });
         res.json({
-            mensaje:"Se actualizo los datos"
+            mensaje: "Se actualizo los datos"
         });
-    } catch(err){
+    } catch (err) {
         res.json({
-            mensaje:" No se pudo actulizar los datos : ERROR:" + err
+            mensaje: " No se pudo actulizar los datos : ERROR:" + err
         });
     }
 }
 
-pagoController.eliminarpedido=async (req,res)=>{
-    await Pago.findByIdAndRemove(req,params.id);
-    res.json({'status':'Pedido eliminado'});
+pagoController.eliminarpedido = async (req, res) => {
+    await Pago.findByIdAndRemove(req, params.id);
+    res.json({ 'status': 'Pedido eliminado' });
 }
 
-module.exports=pagoController;
+module.exports = pagoController;
