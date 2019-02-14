14c8652ba0c0d7e9b13767653ee5e2b48a3c24f8
gabel10 <zapata.gabriel.abel@gmail.com>
2019-02-14 13:25:13 -0500

    modulos de facturacion

--- server/controllers/facturacion.controller.js
+++ server/controllers/facturacion.controller.js
@@ -6,15 +6,67 @@ facturacionController.getApi = async (req, res) => {
     conn.query(consulta, function (err, results) {
       if (err) {
         res.json({
-            mensaje: 'Error'
+          mensaje: 'Error'
         });
       } else {
         res.json({
-            mensaje: results[0].direccion
+          mensaje: results[0].direccion
         });
       }
     })
   });
 };
+
+facturacionController.getNombreCliente = async (req, res) => {
+  req.getConnection(function (error, conn) {
+    var consulta = "SELECT H_fnMan_NombresCliente(?) AS Mensaje";
+    conn.query(consulta, [req.params.idCliente] ,function (err, results) {
+      if (err) {
+        res.json({
+          mensaje: 'Error'
+        });
+      } else {
+        res.json({
+          mensaje: results[0].Mensaje
+        });
+      }
+    })
+  });
+};
+
+facturacionController.getDocumento = async (req, res) => {
+  req.getConnection(function (err, conn) {
+    // Obtener los datos de venta del comprobante
+    var consulta = "call spu_anuven_datosventaarticulos(?,?,?,?)";
+    var valores = [req.params.idLocal, req.params.tipoDoc, req.params.serieDoc, req.params.numeroDoc];
+    conn.query(consulta, valores, function (err, results) {
+      if (err) {
+        res.json({
+          mensaje: 'error'
+        })
+      } else {
+        res.json(results[0][0]);
+      }
+    });
+  });
+}
+
+facturacionController.getDetalleDocumento = async (req, res) => {
+  req.getConnection(function (err, conn) {
+    // Obtener el detalle de venta del comprobante
+    var consultaDetalle = "CALL PA_DetalleVentaWeb(?,?,?)";
+    var valoresCliente = [req.params.tipoDoc, req.params.serieDoc, req.params.numeroDoc];
+    conn.query(consultaDetalle, valoresCliente, function(err, detalle){
+      if(err) {
+        res.json({
+          mensaje: 'Error'
+        })
+      } else {
+        res.json(detalle[0]);
+      }
+    });
+  });
+}
+
 
 module.exports = facturacionController;
--- server/routes/facturacion.route.js
+++ server/routes/facturacion.route.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const facturacion = require('../controllers/facturacion.controller');
 
 router.get('/api', facturacion.getApi);
+router.get('/documento/:idLocal/:tipoDoc/:serieDoc/:numeroDoc', facturacion.getDocumento);
+router.get('/detalle/:tipoDoc/:serieDoc/:numeroDoc', facturacion.getDetalleDocumento);
+router.get('/nombrecliente/:idCliente', facturacion.getNombreCliente);
 
 module.exports = router;
