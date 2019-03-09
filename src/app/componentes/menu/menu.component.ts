307450d6c5c035e3f67e17b0f74ad0675dc9abc1
CesarAchahuiVilca <130341@unsaac.edu.pe>
2019-03-09 12:52:21 -0500

    responsive menu

--- src/app/componentes/menu/menu.component.ts
+++ src/app/componentes/menu/menu.component.ts
@@ -72,11 +72,14 @@ export class MenuComponent implements OnInit {
   openNav() {
     document.getElementById("mySidepanel").style.width = "100%";
     document.getElementById("mySidepanel").style.height = screen.height+"px";
+    document.getElementById("mySidepanel2").style.width = "100%";
+    document.getElementById("mySidepanel2").style.height = screen.height+"px";
   }
 
   /* Set the width of the sidebar to 0 (hide it) */
   closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
+    document.getElementById("mySidepanel2").style.width = "0";
   }
 
   mostrarMenu(opcion){
