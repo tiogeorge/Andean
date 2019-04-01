7a539049a35c0b03c025c4fda8beb3405b657fe3
gabel10 <zapata.gabriel.abel@gmail.com>
2019-04-01 13:30:10 -0500

    POST de cargo

--- server/controllers/pasarela.controller.js
+++ server/controllers/pasarela.controller.js
@@ -2,8 +2,23 @@ const pasarelaController = {};
 
 pasarelaController.crearPago = async(req, res) => {
     console.log(req.body);
+    const settings = {
+       method: 'POST',
+       headers:{
+          'Content-type': 'application/json',
+          'Authorization': 'Bearer << llave privada >>'
+       },
+       body : {
+          'amount' : '10000',
+          'currency_code': 'PEN',
+          'email' : 'gabriel@gmail.com',
+          'source_id': req.body.token
+       }
+    }
+    const response = await fetch(' https://api.culqi.com/v2/charges', settings);
+    let data = await response.json();
     res.json({
-       msg: 'Creando cargo'
+       msg: data
     });
 }
 
