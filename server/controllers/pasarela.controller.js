5f510c2322549cb5aa51c8c83edb1c2dbb3da9c6
gabel10 <zapata.gabriel.abel@gmail.com>
2019-04-03 13:31:00 -0500

    detalles pasarela

--- server/controllers/pasarela.controller.js
+++ server/controllers/pasarela.controller.js
@@ -1,33 +1,42 @@
 const pasarelaController = {};
 
-pasarelaController.crearPago = async(req, res) => {
-    console.log(req.body);
-    const settings = {
-       method: 'POST',
-       headers:{
-          'Content-type': 'application/json',
-          'Authorization': 'Bearer << llave privada >>'
-       },
-       body : {
-          'amount' : req.body.precio,
-          'currency_code': 'PEN',
-          'email' : req.body.email,
-          'source_id': req.body.token
-       }
+pasarelaController.crearPago = async (req, res) => {
+  console.warn('==============================================================');
+  console.log(req.body);
+  console.warn('==============================================================');
+  const settings = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'Authorization': 'Bearer << llave privada >>'
+    },
+    body: {
+      'amount': req.body.precio,
+      'currency_code': 'PEN',
+      'email': req.body.email,
+      'source_id': req.body.token
     }
-    try {
-      const response = await fetch('https://api.culqi.com/v2/charges', settings);
-      let data = await response.json();
-      res.json({
-         msg: data
-      });
-    }catch(e){
-       res.json({
-          status: false,
-          error: e
-       })
-    }
-    
+  }
+  try {
+    const response = await fetch('https://api.culqi.com/v2/charges', settings);
+    let data = await response.json();
+    console.warn('==============================================================');
+    console.log(data);
+    console.warn('==============================================================');
+    res.json({
+      status: true,
+      msg: data
+    });
+  } catch (err) {
+    console.warn('==============================================================');
+    console.log(err);
+    console.warn('==============================================================');
+    res.json({
+      status: false,
+      error: err
+    })
+  }
+
 }
 
 module.exports = pasarelaController;
